406ee217faa7173d88a854c2b579f1b3f2be2216
jessekornblum <jessekornblum@d1b3761f-3242-0410-b1a5-fe525d664610>
2012-04-11 11:50:20 +0000

    Experimental conversion to C++

---
+++ branches/indexing/trunk/ssdeep.h
@@ -0,0 +1,25 @@
+
+/* Fuzzy Hashing by Jesse Kornblum
+   Copyright (C) 2008 ManTech International Corporation
+
+   $Id$ 
+*/
+
+#ifndef __SSDEEP_H
+#define __SSDEEP_H
+
+#include "main.h"
+
+/* RBF - Resolve __progname
+#ifdef __GLIBC__
+extern char *__progname;
+#else 
+char *__progname;
+#endif
+*/
+
+#define __progname "ssdeep"
+
+
+
+#endif  // #ifndef __SSDEEP_H
